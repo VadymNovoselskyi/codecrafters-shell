fa8158599e491d76cd15b994aced930aa5267070
Vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2026-02-21 14:43:59 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -8,17 +8,23 @@ const rl = createInterface({
 
 rl.prompt();
 
-rl.on("line", (command) => {
+rl.on("line", (commandsStr) => {
+  const [command, ...args] = commandsStr.split(" ");
+  
   switch (command) {
     case "exit":
       rl.close();
       break;
+    case "echo":
+      console.log(args.join(" "));
+      rl.prompt();
+      break;
     default:
       console.log(`${command}: command not found`);
       rl.prompt();
-  }
+    }
 });
 
-rl.on('close', () => {
-  process.exit(0)
-})
+rl.on("close", () => {
+  process.exit(0);
+});
