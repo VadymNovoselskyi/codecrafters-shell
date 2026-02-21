4f0b5d1afaf8298968bb62d3cb235e9e3ed74e1b
Vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2026-02-21 15:09:18 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import { createInterface } from "readline";
+import fs from "fs";
 
 const builtins = ["echo", "exit", "type"];
 const rl = createInterface({
@@ -21,11 +22,38 @@ rl.on("line", (commandsStr) => {
       rl.prompt();
       break;
     case "type":
-      if (builtins.includes(args[0])) {
-        console.log(`${args[0]} is a shell builtin`);
+      let commandFound = false;
+      const searchedCommand = args[0];
+
+      if (builtins.includes(searchedCommand)) {
+        console.log(`${searchedCommand} is a shell builtin`);
+        commandFound = true;
       } else {
-        console.log(process.env.PATH)
-        console.log(`${args[0]}: command not found`);
+        const paths = process.env.PATH?.split(":");
+        if (!paths) {
+          break;
+        }
+
+        for (const path of paths) {
+          if (!fs.existsSync(path)) continue;
+
+          const contents = fs.readdirSync(path);
+          if (!contents.includes(searchedCommand)) continue;
+
+          try {
+            fs.accessSync(`${path}/${searchedCommand}`, fs.constants.X_OK);
+          } catch {
+            continue;
+          }
+
+          commandFound = true
+          console.log(`${searchedCommand} is ${path}/${searchedCommand}`);
+          break;
+        }
+      }
+
+      if (!commandFound) {
+        console.log(`${searchedCommand}: not found`);
       }
       rl.prompt();
       break;
